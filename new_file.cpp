d053c79e0b042ac88dbd493fced59af73e39e7d8
Colin Clang <cclang@nic.edu>
2023-10-25 10:41:25 -0700

    BANG BANG BANG

--- new_file.cpp
+++ new_file.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
+#include <chrono>
+#include <thread>
 using namespace std;
 
 int main(){
     cout << "Goodbye world\n";
+    char answer;
+    cout << "Do you wish to continue?(y/n)";
+    cin >> answer;
+    if (answer == 'n')
+    {
+        cout << "Bummer...";
+    }
+    else 
+    {
+    cout << "Goodbye cruel world\n";
     while (1)
     {
         cout << "*BANG*\n";
+    }  
     }
     return 0;
 }
